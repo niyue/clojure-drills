699b71f70f639d3dbaccc959c24013a54f0cb93f
Yue Ni <niyue.com@gmail.com>
2013-11-07 11:20:31 +0800

    Solve the 'duplicate a sequence' problem in 4clojure.

---
+++ test/clojure_drills/4clojure/duplicate_a_sequence_test.clj
@@ -0,0 +1,18 @@
+; http://www.4clojure.com/problem/32
+
+(ns clojure-drills.4clojure.duplicate_a_sequence_test
+  (:use clojure.test
+        clojure-drills.core))
+
+
+(deftest set-function
+  (testing "use set as a function will return element itself instead of a boolean value"
+    (is (= 7 (#{2 7 6} 7)))))
+
+(deftest duplicate-a-sequence
+  (is (= [1 1 2 2 3 3]
+         (#(interleave % %) [1 2 3]))))
+
+;(interleave [1 2 3] [1 2 3])
+;(apply concat (map #(list % %) [1 2 3]))
+;(mapcat #list(% %) [1 2 3])
