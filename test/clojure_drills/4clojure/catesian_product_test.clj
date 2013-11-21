8ccc1b907e3fff73f8ae55606c666a6d3d67ffdc
Yue Ni <niyue.com@gmail.com>
2013-11-21 15:58:41 +0800

    Implement catesian product in 4clojure.com.

---
+++ test/clojure_drills/4clojure/catesian_product_test.clj
@@ -0,0 +1,12 @@
+; http://www.4clojure.com/problem/90
+
+(ns clojure-drills.4clojure.catesian-product-test
+  (:use clojure.test
+        clojure-drills.core))
+
+(defn catesian-product [a b]
+  (set (#(for [x % y %2] [x y]) a b)))
+
+(deftest catesian-product-test
+  (is (= (catesian-product #{1 2 3} #{4 5})
+         #{[1 4] [2 4] [3 4] [1 5] [2 5] [3 5]})))
