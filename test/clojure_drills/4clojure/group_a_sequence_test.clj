c495ca2d5748b566b6f04a132498c70e875a11f0
Yue Ni <niyue.com@gmail.com>
2013-11-23 22:28:12 +0800

    Solve the group a sequence problem in 4clojure.com.

---
+++ test/clojure_drills/4clojure/group_a_sequence_test.clj
@@ -0,0 +1,14 @@
+; http://www.4clojure.com/problem/63
+
+(ns clojure-drills.4clojure.group-a-sequence-test
+  (:use clojure.test
+        clojure-drills.core))
+
+(defn group [f coll]
+  (reduce #(assoc % (f %2) (concat (% (f %2)) (vector %2))) {} coll))
+
+(deftest group-a-sequence-test
+    (is (= (group #(> % 5) [1 3 6 8]) {false [1 3], true [6 8]})))
+
+; learn:
+; 1) merge-with
