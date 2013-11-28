91bd771217ed65d682447a113ef3f556c15d119d
Yue Ni <niyue.com@gmail.com>
2013-11-28 15:51:15 +0800

    Figure out how to determine if a collection is a set/map/list/vector in 4clojure.com.

---
+++ test/clojure_drills/4clojure/black_box_testing_test.clj
@@ -0,0 +1,21 @@
+; http://www.4clojure.com/problem/102
+
+(ns clojure-drills.4clojure.into-camel-case-test
+  (:use clojure.test
+        clojure-drills.core))
+
+(defn camelize [s]
+  (let [[a & xs] (.split #"-" s)]
+    (apply str a (map clojure.string/capitalize xs))))
+
+(defn which-coll [c]
+  (cond
+    (reversible? c) :vector
+    (associative? c) :map
+    (ifn? c) :set
+    :else :list))
+
+(deftest which-collection-test
+    (is (= [:map :set :vector :list] (map which-coll [{} #{} [] ()]))))
+
+
