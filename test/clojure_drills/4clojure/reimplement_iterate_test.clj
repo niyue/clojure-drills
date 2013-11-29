21056d934c8d00bf293889802f0a75cf114f888a
Yue Ni <niyue.com@gmail.com>
2013-11-29 16:35:27 +0800

    Solve the reimplement iterate problem in 4clojure.com.

---
+++ test/clojure_drills/4clojure/reimplement_iterate_test.clj
@@ -0,0 +1,15 @@
+; http://www.4clojure.com/problem/62
+
+(ns clojure-drills.4clojure.reimplement-iterate-test
+  (:use clojure.test
+        clojure-drills.core))
+
+(defn new-iterate [f x]
+  (lazy-seq
+   (cons x (new-iterate f (f x)))))
+
+(deftest inc-iterate-test
+  (is (= (take 100 (range 10)) (take 100 (new-iterate inc 0)))))
+
+; learn
+; 1) lazy-seq
