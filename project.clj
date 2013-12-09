e04e4dd72b051ddffea353ab21f49ec331f84e09
Yue Ni <niyue.com@gmail.com>
2013-12-09 13:57:12 +0800

    Add clojure math library for this project.

--- project.clj
+++ project.clj
@@ -1,6 +1,7 @@
-(defproject clojure-drills "0.1.0-SNAPSHOT"
+(defproject clojure-drills "0.2.0-SNAPSHOT"
   :description "Coding drills for learning Clojure"
   :url "http://niyue.github.com/clojure-drills"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]])
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/math.numeric-tower "0.0.2"]])
