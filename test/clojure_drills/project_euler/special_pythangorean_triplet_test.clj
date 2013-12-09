b3d94ebf59181da9300b56099f08475da8c09485
Ni Yue <niyue.com@gmail.com>
2013-12-09 21:37:37 +0800

    Find a pythangorean triple which sums to 1000.

---
+++ test/clojure_drills/project_euler/special_pythangorean_triplet_test.clj
@@ -0,0 +1,15 @@
+; http://projecteuler.net/problem=9
+
+(ns clojure-drills.project-euler.special-pythangorean-triple-test
+  (:require [clojure.math.numeric-tower :as math])
+  (:use clojure.test
+        clojure-drills.core))
+
+(defn pythgorean [sum]
+  (for [a (range 1 (inc sum))
+        b (range (inc a) (inc sum))
+        :let [c (- sum a b)]
+        :when (= (+ (* a a) (* b b)) (* c c))]
+    [a b c]))
+
+(reduce * (first (pythgorean 1000)))
