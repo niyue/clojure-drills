8d72c14856f7385ba22203e15cafe1d53e631ba4
Yue Ni <niyue.com@gmail.com>
2013-12-09 13:56:35 +0800

    Initial implementation for calculating prime factors. It is too slow to do prime sieve for 770k numbers.

--- test/clojure_drills/project_euler/biggest_prime_factors_test.clj
+++ test/clojure_drills/project_euler/biggest_prime_factors_test.clj
@@ -5,11 +5,25 @@
   (:use clojure.test
         clojure-drills.core))
 
-(math/sqrt 1000)
+(defn boolean-vec [x]
+  (vec (repeat (inc x) true)))
+
+(defn sieve [is-prime i]
+  (let [square (* i i)
+        [n1 n2] (split-at square is-prime)
+        n (count is-prime)]
+    (vec (concat n1
+            (map #(if (zero? (mod %2 i)) false %)
+                 n2
+                 (map #(+ square %) (range n)))))))
 
 (defn primes [n]
-  [1 2 3])
+  (loop [is-prime (assoc (assoc (boolean-vec n) 0 false) 1 false)
+         i 2]
+    (cond (> i n)                (filter #(nth is-prime %) (range 2 (inc n)))
+          (not (nth is-prime i)) (recur is-prime (inc i))
+          :else                  (recur (sieve is-prime i) (inc i)))))
 
-(some
- #(zero? (mod 600851475143 %))
- (reverse (primes 600851475143)))
+
+;(primes 10000)
+;(apply max (primes (math/sqrt 600851475143)))
