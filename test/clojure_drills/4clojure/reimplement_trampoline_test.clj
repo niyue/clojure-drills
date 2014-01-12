da072bbd3eca76a6e364b225da1a043da889e247
Ni Yue <niyue.com@gmail.com>
2014-01-12 22:42:07 +0800

    Keep on solving the trampoline problem but was not able to solve it yet, need to take a further look at its definition.

--- test/clojure_drills/4clojure/reimplement_trampoline_test.clj
+++ test/clojure_drills/4clojure/reimplement_trampoline_test.clj
@@ -4,15 +4,23 @@
   (:use clojure.test))
 
 (defn trpl [f & args]
-  (loop [r f]
+  (loop [r f
+         params args]
     (if (fn? r)
-      (if (empty? args)
-        (recur (r args))
-        (recur (r [])))
+      (if (nil? params)
+        (recur (r) args)
+        (recur (r args) nil))
       r)))
 
-(deftest reimplement-trampoline-test
+(deftest reimplement-trampoline-calc-test
+  (= (letfn [(triple [x] #(sub-two (* 3 x)))
+          (sub-two [x] #(stop?(- x 2)))
+          (stop? [x] (if (> x 50) x #(triple x)))]
+       (trpl triple 2))
+     82))
+
+(deftest reimplement-trampoline-myeven-test
   (= (letfn [(my-even? [x] (if (zero? x) true #(my-odd? (dec x))))
           (my-odd? [x] (if (zero? x) false #(my-even? (dec x))))]
-    (map (partial trpl my-even?) (range 6)))
-  [true false true false true false]))
+       (map (partial trpl my-even?) (range 6)))
+     [true false true false true false]))
