c608f2ab38942a6658ac2a666f1f459fe51a938b
Ni Yue <niyue.com@gmail.com>
2014-01-21 21:59:44 +0800

    Tried to solve this problem using Clojure’s memoize function but not managed to do it yet.

--- test/clojure_drills/project_euler/lattice_paths_test.clj
+++ test/clojure_drills/project_euler/lattice_paths_test.clj
@@ -1,16 +1,18 @@
 ; http://projecteuler.net/problem=15
 
-(ns clojure-drills.project-euler.sum-square-difference-test
+(ns clojure-drills.project-euler.lattice-path-test
   (:require [clojure.math.numeric-tower :as math])
   (:use clojure.test
         clojure-drills.core))
 
 
 (defn paths [x y]
-  (if (or (zero? x) (zero? y))
-    1
-    (+ (paths (dec x) y) (paths x (dec y)))))
+  ((memoize
+    (fn []
+      (if (or (zero? x) (zero? y))
+        1
+        (+ (paths (dec x) y) (paths x (dec y))))))))
 
-;(paths 2 2)
+;(paths 10 20)
 
 
