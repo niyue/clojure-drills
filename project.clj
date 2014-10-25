3a35dd13f8e21c49fbff991f92513d76c81e79da
Ni Yue <niyue.com@gmail.com>
2014-10-25 15:01:56 +0800

    Upgrade clojure to 1.7 to use transducer if possible.

--- project.clj
+++ project.clj
@@ -3,5 +3,5 @@
   :url "http://niyue.github.com/clojure-drills"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.7.0-alpha2"]
                  [org.clojure/math.numeric-tower "0.0.2"]])
