13b3dcb84859cb0462f6729535c8757a241f1238
Ni Yue <niyue.com@gmail.com>
2014-10-25 21:31:43 +0800

    Solve the 'reverse a sequence' problem using a different solution.

---
+++ test/clojure_drills/4clojure/reverse_a_sequence_test.clj
@@ -0,0 +1,18 @@
+; http://www.4clojure.com/problem/23
+
+(ns clojure-drills.4clojure.reverse-a-sequence-test
+  (:use clojure.test
+        clojure-drills.core))
+
+(defn rev [coll]
+  (loop [r []
+         c coll]
+    (if (seq c)
+      (recur (concat [(first c)] r) (rest c))
+      r)))
+
+(deftest reverse-test
+    (is (= [1 2 3] (rev [3 2 1]))))
+
+; learn:
+; 1) how to append an element into the end of a sequence `concat coll [e]`
