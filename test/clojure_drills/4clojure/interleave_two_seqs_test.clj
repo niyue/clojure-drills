26051f9b0982dc57ec408c20e0a79ba1b6315130
Ni Yue <niyue.com@gmail.com>
2014-10-26 11:34:10 +0800

    Add another implementation for `interleave` using `mapcat`.

--- test/clojure_drills/4clojure/interleave_two_seqs_test.clj
+++ test/clojure_drills/4clojure/interleave_two_seqs_test.clj
@@ -4,11 +4,18 @@
   (:use clojure.test
         clojure-drills.core))
 
+(defn intl [c1 c2]
+  (apply concat (map #(list %1 %2) c1 c2)))
+
+
 (deftest interleave-two-seqs
     (is (=
          '(1 :a 2 :b 3 :c)
          ((fn intl [c1 c2]
            (apply concat (map #(list %1 %2) c1 c2))) [1 2 3] [:a :b :c]))))
+
+(deftest interleave-two-seqs-test
+    (is (= '(1 :a 2 :b 3 :c) (intl [1 2 3] [:a :b :c]))))
 
 
 
