4c3a6bea9c6d5533c28d8ee1cd0c8874624c2af7
Ni Yue <niyue.com@gmail.com>
2014-10-26 12:12:19 +0800

    Add an alternative implementation for drop-nth using keep-indexed.

--- test/clojure_drills/4clojure/drop_every_nth_item_test.clj
+++ test/clojure_drills/4clojure/drop_every_nth_item_test.clj
@@ -11,6 +11,17 @@ zero? (mod 10 3)
     #(not (zero? (mod (inc (first %)) n)))
     (map #(list % %2) (range) coll))))
 
+(defn drop-n [coll n]
+  (keep-indexed #(if-not (zero? (mod (inc %1) n)) %2)
+                coll))
+
 (deftest drop-every-nth-item-test
   (is (= [1 2 4 5 7 8]
          (drop-nth [1 2 3 4 5 6 7 8] 3))))
+
+(deftest drop-every-nth-item-test-two
+  (is (= [1 2 4 5 7 8]
+         (drop-n [1 2 3 4 5 6 7 8] 3))))
+
+; learn:
+; 1) keep-indexed usage
