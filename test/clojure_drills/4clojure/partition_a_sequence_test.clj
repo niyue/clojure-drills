50eb714778d90566b8b27833f6e580c7497c11d1
Ni Yue <niyue.com@gmail.com>
2014-10-29 22:00:07 +0800

    Implement a general solution for the partition a sequence problem using loop recur.

--- test/clojure_drills/4clojure/partition_a_sequence_test.clj
+++ test/clojure_drills/4clojure/partition_a_sequence_test.clj
@@ -17,9 +17,19 @@
       p
       (drop-last p))))
 
+; a general solution to this problem (the elements in the coll does not need to be an integer)
+(defn partition-seq-3 [n coll]
+  (loop [c coll
+         r []]
+    (if (> n (count c))
+      r
+      (recur (drop n c) (conj r (take n c))))))
+
 (deftest partition-seq-test
   (is (= (partition-seq 3 (range 8)) '((0 1 2) (3 4 5)))))
 
 (deftest partition-seq-2-test
   (is (= (partition-seq-2 3 (range 8)) '((0 1 2) (3 4 5)))))
 
+(deftest partition-seq-3-test
+  (is (= (partition-seq-3 3 (range 8)) '((0 1 2) (3 4 5)))))
