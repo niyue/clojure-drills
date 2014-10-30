387013eaf8fccb236110e0b5afef9593b1f275ab
Ni Yue <niyue.com@gmail.com>
2014-10-30 22:48:45 +0800

    Solve the map construction problem using into function.

--- test/clojure_drills/4clojure/map_construction_test.clj
+++ test/clojure_drills/4clojure/map_construction_test.clj
@@ -10,6 +10,14 @@
    {}
    (map #(vector %1 %2) k v)))
 
+
+(defn map-construction [k v]
+  (into {} (map #(vector %1 %2) k v)))
+
 (deftest map-construction-test
     (is (= {:a 1, :b 2}
            (mc [:a :b] [1 2]))))
+
+(deftest map-construction-test-two
+    (is (= {:a 1, :b 2}
+           (map-construction [:a :b] [1 2]))))
