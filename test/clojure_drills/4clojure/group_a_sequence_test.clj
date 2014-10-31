8eeb5d16d72bdceffb5c21e57ce68d93c79ed9b1
Ni Yue <niyue.com@gmail.com>
2014-10-31 21:35:20 +0800

    Simplify the solution for group a sequence a little bit.

--- test/clojure_drills/4clojure/group_a_sequence_test.clj
+++ test/clojure_drills/4clojure/group_a_sequence_test.clj
@@ -7,8 +7,14 @@
 (defn group [f coll]
   (reduce #(assoc % (f %2) (concat (% (f %2)) (vector %2))) {} coll))
 
+(defn grp [f coll]
+  (reduce #(assoc % (f %2) (conj (vec (% (f %2))) %2)) {} coll))
+
 (deftest group-a-sequence-test
     (is (= (group #(> % 5) [1 3 6 8]) {false [1 3], true [6 8]})))
+
+(deftest group-a-sequence-test
+    (is (= (grp #(> % 5) [1 3 6 8]) {false [1 3], true [6 8]})))
 
 ; learn:
 ; 1) merge-with
