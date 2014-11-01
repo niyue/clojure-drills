fbbd30b36912adb62f3266eb5ec312952ded91e3
Ni Yue <niyue.com@gmail.com>
2014-11-01 12:58:57 +0800

    Add two alternative implementation for count occurrences.

--- test/clojure_drills/4clojure/count_occurences_test.clj
+++ test/clojure_drills/4clojure/count_occurences_test.clj
@@ -8,5 +8,21 @@
   (let [counts (group-by identity coll)]
     (zipmap (keys counts) (map count (vals counts)))))
 
+(defn count-occurences [coll]
+  (let [counts (group-by identity coll)]
+    (into {} (for [[k v] counts] [k (count v)]))))
+
+(defn count-occurences-2 [coll]
+  (let [counts (group-by identity coll)
+        counts-vec (map #(vector (first %) (count (second %))) counts)]
+    (into {} counts-vec)))
+
 (deftest count-occurences-test
   (is (= {1 4, 2 2, 3 1} (count-occ [1 1 2 3 2 1 1]))))
+
+(deftest count-occurences-test-two
+  (is (= {1 4, 2 2, 3 1} (count-occurences [1 1 2 3 2 1 1]))))
+
+(deftest count-occurences-test-three
+  (is (= {1 4, 2 2, 3 1} (count-occurences-2 [1 1 2 3 2 1 1]))))
+
