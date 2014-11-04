7da02bc797aa4fae4f93f574866f579ce32cda27
Ni Yue <niyue.com@gmail.com>
2014-11-04 08:53:25 +0800

    Use set difference to solve the symmetric difference problem.

--- test/clojure_drills/4clojure/symmetric_difference_test.clj
+++ test/clojure_drills/4clojure/symmetric_difference_test.clj
@@ -9,5 +9,17 @@
         i (clojure.set/intersection a b)]
     (filter #(not (contains? i %)) u))))
 
+(defn diff-2 [a b]
+  (let [u (clojure.set/union a b)
+        i (clojure.set/intersection a b)]
+    (clojure.set/difference u i)))
+
 (deftest symmetric-differecen-test
     (is (= (diff #{1 2 3 4 5 6} #{1 3 5 7}) #{2 4 6 7})))
+
+(deftest symmetric-differecen-test-two
+    (is (= (diff-2 #{1 2 3 4 5 6} #{1 3 5 7}) #{2 4 6 7})))
+
+; learn:
+; 1) complement
+; 2) disj
