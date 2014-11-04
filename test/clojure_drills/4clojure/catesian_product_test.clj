0f0c62dbe6e63ea09a860bc04013ae56ca7c306c
Ni Yue <niyue.com@gmail.com>
2014-11-04 22:19:32 +0800

    Add an simpler implementation for catesian product.

--- test/clojure_drills/4clojure/catesian_product_test.clj
+++ test/clojure_drills/4clojure/catesian_product_test.clj
@@ -7,6 +7,12 @@
 (defn catesian-product [a b]
   (set (#(for [x % y %2] [x y]) a b)))
 
+(defn cp [a b]
+  (set (for [x a y b] [x y])))
+
 (deftest catesian-product-test
   (is (= (catesian-product #{1 2 3} #{4 5})
          #{[1 4] [2 4] [3 4] [1 5] [2 5] [3 5]})))
+
+(deftest catesian-product-test-two
+  (is (= (cp #{1 2 3} #{4 5}) #{[1 4] [2 4] [3 4] [1 5] [2 5] [3 5]})))
