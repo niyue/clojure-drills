1d9d5d6d49eea02b50751fbf4d77de3f7827070d
Ni Yue <niyue.com@gmail.com>
2014-11-09 22:26:27 +0800

    Solve the big divide problem in 4clojure.

--- test/clojure_drills/4clojure/the_big_divide_test.clj
+++ test/clojure_drills/4clojure/the_big_divide_test.clj
@@ -15,6 +15,14 @@
     (- (+ (multiple-sum n a) (multiple-sum n b))
        (multiple-sum n (* a b)))))
 
+(defn the-big-divide [n a b]
+  (let [c (dec n)
+        sum (fn [m x]
+              (let [y (quot m x)
+                    s (/ (*' y (inc y)) 2)]
+                (*' s x)))]
+    (-' (+' (sum c a) (sum c b)) (sum c (*' a b)))))
+
 (deftest no-qualified-number-test
     (is (= 0 (big-divide 3 17 11))))
 
@@ -24,5 +32,11 @@
 (deftest medium-number-test
     (is (= 233168 (big-divide 1000 3 5))))
 
+(deftest number-23-test
+    (is (= 23 (the-big-divide 10 3 5))))
+
 (deftest large-number-test
-    (is (= "2333333316666668" (str (big-divide 100000000 3 5)))))
+    (is (= "2333333316666668" (str (the-big-divide 100000000 3 5)))))
+
+(deftest interge-overflow-test
+    (is (= "110389610389889610389610" (str (the-big-divide (* 10000 10000 10000) 7 11)))))
